47ac922e9e8c44918c0ad6e04bffc35567dfbc40
IrfanHos02 <houseirfan02@gmail.com>
2022-05-08 01:38:51 +0700

    menambahkan fitur lokal host wifi

--- SMARTLAMPBLYNK.ino
+++ SMARTLAMPBLYNK.ino
@@ -5,6 +5,7 @@
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <WiFiManager.h>
 #define indikator_1 D7 //kuning
 #define indikator_2 D0 //hijau
 
@@ -15,6 +16,7 @@ char pass[] = "irfan0204";
 unsigned long Sindi = 0;
 int Delay = 500;
 bool stateInd = false;
+bool connectWIFI;
 
 BLYNK_WRITE(V0)
 {
@@ -27,16 +29,23 @@ BLYNK_WRITE(V0)
 }
 
 void setup() {
+   WiFi.mode(WIFI_STA);
     Serial.begin(9600);
     pinMode(D7,OUTPUT);
     pinMode(D0,OUTPUT);
-     if(WiFi.status() != WL_CONNECTED){
-       digitalWrite(D7,HIGH);
-       //warningIndikator();
-       Serial.println("WIFI NOT CONNECT");
-     }
+
+   WiFiManager wifi;
+   connectWIFI = wifi.autoConnect("CONTROLLER","00000000"); // password protected ap
+    if(!connectWIFI) {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    } 
+    else {
+        //if you get here you have connected to the WiFi    
+        Serial.println("connected...yeey :)");
+    }
     delay(100);
-    Blynk.begin(auth,ssid,pass);
+    Blynk.begin(auth,WiFi.SSID().c_str(), WiFi.psk().c_str());
     digitalWrite(D7,LOW);
     Serial.println("READY");
     Blynk.notify("SMART HOME AKTIF");
