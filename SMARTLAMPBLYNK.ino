ab664116b4c40ac156004f9099422c9d1d53d1b5
IrfanHos02 <houseirfan02@gmail.com>
2022-05-08 10:07:53 +0700

    menambahkan fitur lokal host wifi dan memperbaiki bug program dan indikator

--- SMARTLAMPBLYNK.ino
+++ SMARTLAMPBLYNK.ino
@@ -1,27 +1,31 @@
 /*
- * DIBUAT TANGGAL 15-04-2022
- * JAM 16:47
- */
+   DIBUAT TANGGAL 15-04-2022
+   JAM 16:47
+*/
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <WiFiManager.h>
 #define indikator_1 D7 //kuning
 #define indikator_2 D0 //hijau
+#define ResetAP D6
 
 char auth[] = "UGMEtVh84Gk4e_x6zMkO2GCqIpFBDIsj";
-char ssid[] = "Irfan.A";
-char pass[] = "irfan0204";
+//char ssid[] = "Irfan.A";
+//char pass[] = "irfan0204";
 
 unsigned long Sindi = 0;
 int Delay = 500;
+int counterWIFI = 0;
 bool stateInd = false;
 bool connectWIFI;
+
+WiFiManager wifi;
 
 BLYNK_WRITE(V0)
 {
   byte pinValue = param.asInt();
-  if(pinValue == 1){
+  if (pinValue == 1) {
     Blynk.notify("CONTROLLER DIRESET");
     delay(200);
     ESP.restart();
@@ -29,55 +33,87 @@ BLYNK_WRITE(V0)
 }
 
 void setup() {
-   WiFi.mode(WIFI_STA);
-    Serial.begin(9600);
-    pinMode(D7,OUTPUT);
-    pinMode(D0,OUTPUT);
+  WiFi.mode(WIFI_STA);
+  Serial.begin(9600);
+  pinMode(indikator_1, OUTPUT);
+  pinMode(indikator_2, OUTPUT);
+  pinMode(D6, INPUT);
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D4, HIGH);
+  digitalWrite(D5, HIGH);
 
-   WiFiManager wifi;
-   connectWIFI = wifi.autoConnect("CONTROLLER","00000000"); // password protected ap
-    if(!connectWIFI) {
-        Serial.println("Failed to connect");
-        // ESP.restart();
-    } 
-    else {
-        //if you get here you have connected to the WiFi    
-        Serial.println("connected...yeey :)");
-    }
-    delay(100);
-    Blynk.begin(auth,WiFi.SSID().c_str(), WiFi.psk().c_str());
-    digitalWrite(D7,LOW);
-    Serial.println("READY");
-    Blynk.notify("SMART HOME AKTIF");
+  // wifi.setConfigPortalBlocking(false);
+  //wifi.setConfigPortalTimeout(100);
+
+  connectWIFI = wifi.autoConnect("CONTROLLER", "00000000"); // password protected ap
+  if (!connectWIFI) {
+    Serial.println("Failed to connect");
+    digitalWrite(indikator_1, HIGH);
+    digitalWrite(indikator_2, HIGH);
+    // ESP.restart();
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(indikator_1, HIGH);
+    Serial.println("WIFI NOT CONNECTED");
+  }
+
+  Serial.println("connected...");
+  /////////// delay(100);
+  Blynk.begin(auth, WiFi.SSID().c_str(), WiFi.psk().c_str());
+  digitalWrite(indikator_1, LOW);
+  Serial.println("READY");
+  Blynk.notify("SMART HOME AKTIF");
 }
 
 void loop() {
-    cekWIFI();
-    digitalWrite(D0,stateInd);
-    Blynk.run();
-    
+
+  cekWIFI();
+  resetWifiAP();
+  digitalWrite(indikator_2, stateInd);
+  Blynk.run();
+
 }
 
-void cekWIFI(){
-  
-   if(WiFi.status() != WL_CONNECTED){
-       digitalWrite(D7,HIGH);
-       warningIndikator();
-       Serial.println("WIFI DISCONNECT");
-     }
+void cekWIFI() {
 
-    else
-    {
-      digitalWrite(D7,LOW);
-      stateInd = true;
-    }
-    
+  if (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(indikator_1, HIGH);
+    warningIndikator();
+    Serial.println("WIFI DISCONNECT");
+  }
+
+  else
+  {
+    digitalWrite(indikator_1, LOW);
+    stateInd = true;
+  }
+
 }
 
-void warningIndikator(){
+void warningIndikator() {
   unsigned long tmr = millis();
-  if(tmr - Sindi > Delay){
+  if (tmr - Sindi > Delay) {
     Sindi = tmr;
     stateInd = !stateInd;
+    counterWIFI++;
   }
+  //Serial.println(counterWIFI);
+  if (counterWIFI >= 20) {
+    ESP.restart();
+  }
+}
+
+void resetWifiAP() {
+  byte pin = digitalRead(ResetAP);
+
+  if (pin == HIGH) {
+    delay(1000);
+    Serial.println("CONFIG WIFI DI RESET");
+    wifi.resetSettings();
+    delay(3000);
+    ESP.restart();
+  }
+
 }
